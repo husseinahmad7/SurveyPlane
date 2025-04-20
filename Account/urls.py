0855ed7e60a53b7bcad2905034cf877838182efb
Your Name <you@example.com>
2025-04-20 15:40:24 +0300

    fix confirm url in email with output

--- Account/urls.py
+++ Account/urls.py
@@ -3,5 +3,9 @@ from . import views
 
 urlpatterns = [
     path('respondent_signup/', views.RespondentSignup.as_view(), name='respondent_signup'),
-    path('respondent_signin/', views.RespondentLogin.as_view(), name='respondent_signin')
+    path('respondent_signin/', views.RespondentLogin.as_view(), name='respondent_signin'),
+
+    # Override the default authemail verification views
+    path('signup/verify/', views.SignupVerify.as_view(), name='signup_verify'),
+    path('password/reset/verify/', views.PasswordResetVerify.as_view(), name='password_reset_verify'),
 ]
